83e5acf04769200765f6ec0370a3c0eb7d291e4f
Rignu1503 <rigoberto.miranda1503@gmail.com>
2025-10-19 18:40:33 -0500

    feat(msvc-enegy): intial msvc-energy

---
+++ msvc-energy/src/main/java/dev/ener_track/com/demo/MsvcEnergyApplication.java
@@ -0,0 +1,13 @@
+package dev.ener_track.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcEnergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcEnergyApplication.class, args);
+	}
+
+}
---
+++ msvc-energy/src/test/java/dev/ener_track/com/demo/MsvcEnergyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ener_track.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcEnergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
