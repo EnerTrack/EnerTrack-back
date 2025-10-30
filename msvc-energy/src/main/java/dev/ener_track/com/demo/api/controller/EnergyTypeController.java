78bdccc8e63b6129ec5f09ffb3be0c018eee4027
Rignu1503 <rigoberto.miranda1503@gmail.com>
2025-10-30 18:14:31 -0500

    feat(msvc-energy): add person in find by id

--- msvc-energy/src/main/java/dev/ener_track/com/demo/api/controller/EnergyTypeController.java
+++ msvc-energy/src/main/java/dev/ener_track/com/demo/api/controller/EnergyTypeController.java
@@ -12,7 +12,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/energy-types")
+@RequestMapping("/energy-type")
 @AllArgsConstructor
 public class EnergyTypeController {
 
