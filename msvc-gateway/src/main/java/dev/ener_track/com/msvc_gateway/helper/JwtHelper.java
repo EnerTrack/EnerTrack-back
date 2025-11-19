6777a993f6edd3f54da0e45154df5956ab238bcd
Rignu1503 <rigoberto.miranda1503@gmail.com>
2025-11-18 21:06:26 -0500

    feat(msvc-gateway): add filter security

---
+++ msvc-gateway/src/main/java/dev/ener_track/com/msvc_gateway/helper/JwtHelper.java
@@ -0,0 +1,53 @@
+package dev.ener_track.com.msvc_gateway.helper;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+@Slf4j
+public class JwtHelper {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    public boolean validateToken(String token) {
+        try {
+            final var expirationDate = this.getExpirationDateFromToken(token);
+            return expirationDate.after(new Date());
+        } catch (Exception e) {
+            log.error("Error validating token: {}", e.getMessage());
+            return false;
+        }
+    }
+
+    private Date getExpirationDateFromToken(String token) {
+        return this.getClaimsByToken(token, Claims::getExpiration);
+    }
+
+    private <T> T getClaimsByToken(String token, Function<Claims, T> claimsResolver) {
+        return claimsResolver.apply(this.parseToken(token));
+    }
+
+    private Claims parseToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(this.getSecretKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(this.jwtSecret.getBytes(StandardCharsets.UTF_8));
+    }
+}
+
+
